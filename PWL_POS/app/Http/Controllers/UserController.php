64fe529fa20c4fe89127c13fbeb7afb0208acc88
febriansyah adi nugroho <febriansyahhoho@gmail.com>
2025-03-03 20:34:35 +0700

    commit king

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -29,7 +29,46 @@ class UserController extends Controller
         UserModel::create($data);
         UserModel::where('username', 'customer-1')->update($data);*/
 
-        $users = UserModel::where('username', 'manager9')->firstOrFail();
+        
+        $users = UserModel::all();
         return view('user', ['data' => $users]);
     }
-}
+        public function tambah()
+        {
+          return view('user_tambah');
+        }
+
+        public function tambah_simpan(Request $request)
+        {
+          UserModel::create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => Hash::make('$request->password'),
+            'level_id' =>$request->level_id
+          ]);
+          return redirect('/user');
+        }
+
+        public function ubah($id){
+          $user = UserModel::find($id);
+          return view('user_ubah', ['data' => $user]);
+        }
+
+        public function ubah_simpan($id, Request $request){
+          $users = UserModel::find($id);
+
+          $users->username = $request->username;
+          $users->nama = $request->nama;
+          $users->password = Hash::make('$request->password');
+          $users->level_id = $request->level_id;
+
+          $users->save();
+          return redirect('/user');
+        }
+
+        public function hapus($id){
+          $users = UserModel::find($id);
+          $users->delete();
+          return redirect('/user');
+        }
+      }
