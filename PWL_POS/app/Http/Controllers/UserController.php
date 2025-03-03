02e0b2582f1a87bc0e8dd4fd83705f857a98858c
febriansyah adi nugroho <febriansyahhoho@gmail.com>
2025-03-03 11:34:51 +0700

    kingg

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -10,26 +10,26 @@ class UserController extends Controller
 {
     public function index()
     {
-       // $data = [
-         //   'username' => 'customer-1',
-         //   'nama' => 'Pelanggan',
-          //  'password' => Hash::make('12345'),
-           // 'level_id' => 4
-      //  ];
+       /* $data = [
+          'username' => 'customer-1',
+          'nama' => 'Pelanggan',
+          'password' => Hash::make('12345'),
+          'level_id' => 4
+        ];
 
-     // $data = [
-       // 'nama' => 'Pelanggan Pertama'
-      //];
+      $data = [
+        'nama' => 'Pelanggan Pertama'
+      ];
         $data = [
            'level_id' => 2,
            'username' => 'manager_tiga',
-          'nama' => 'Manager 3',
-           'password' => Hash::make('12345')
-     ];
-       UserModel::create($data);
-     // UserModel::where('username', 'customer-1')->update($data);
+         'nama' => 'Manager 3',
+          'password' => Hash::make('12345')
+      ];
+        UserModel::create($data);
+        UserModel::where('username', 'customer-1')->update($data);*/
 
-        $users = UserModel::all();
+        $users = UserModel::findOr(20, ['username', 'nama'], function (){abort(404);});
         return view('user', ['data' => $users]);
     }
 }
