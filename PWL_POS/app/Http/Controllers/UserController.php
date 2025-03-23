9086c6e8757594961c7c41ca6cda29323427b718
febriansyah adi nugroho <febriansyahhoho@gmail.com>
2025-03-23 22:35:25 +0700

    done kingg

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use App\Models\LevelModel;
 use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Yajra\DataTables\Facades\DataTables;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -43,13 +44,18 @@ public function list (Request $request)
     return DataTables::of($users)
         ->addIndexColumn()
         ->addColumn('aksi', function ($user) { // menambahkan kolom aksi
-            $btn = '<a href="'.url('/user/'.$user->user_id).'" class="btn btn-info btn-sm">Detail</a>';
+           /*$btn = '<a href="'.url('/user/'.$user->user_id).'" class="btn btn-info btn-sm">Detail</a>';
             $btn .= '<a href="'.url('/user/'.$user->user_id.'/edit').'" class="btn btn-warning btn-sm">Edit</a>';
             $btn .= '<form class="d-inline-block" method="POST" action="'.url('/user/'.$user->user_id).'">'.
                         csrf_field().
                         method_field('DELETE').
                         '<button type="submit" class="btn btn-danger btn-sm" onclick="return confirm(\'Apakah Anda yakin menghapus data ini?\');">Hapus</button>'.
-                    '</form>';
+                    '</form>';*/
+                    $btn = '<a href="'.url('user/'.$user->user_id).'" class="btn btn-info btn-sm">Detail</a>';
+                                $btn .= '<button onclick="modalAction(\''.url('/user/' . $user->user_id . 
+                    '/edit_ajax').'\')" class="btn btn-warning btn-sm">Edit</button> '; 
+                                $btn .= '<button onclick="modalAction(\''.url('/user/' . $user->user_id . 
+                    '/delete_ajax').'\')"  class="btn btn-danger btn-sm">Hapus</button> '; 
             return $btn;
         })
         ->rawColumns(['aksi']) // memberitahu bahwa kolom aksi adalah html
@@ -170,4 +176,119 @@ public function list (Request $request)
             return redirect('/user')->with('error', 'Data user gagal dihapus karena masih terdapat tabel lain yang terkait dengan data ini');
         }
     }
-}     
+
+    public function create_ajax()
+    {
+        $level = LevelModel::select('level_id', 'level_nama')->get();
+
+        return view('user.create_ajax')->with('level', $level);
+    }
+
+    public function store_ajax(Request $request)
+    {
+        //cek apakah request berupa ajax
+        if ($request->ajax() || $request->wantsJson()) {
+            $rules = [
+                'level_id' => 'required|integer',
+                'username' => 'required|string|min:3|unique:m_user,username',
+                'nama' => 'required|string|max:100',
+                'password' => 'required|min:6'
+            ];
+
+            //use Illuminate\Support\Facades\Validator;
+            $validator = Validator::make($request->all(), $rules);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => False, 
+                    'message' => 'Validasi Gagal',
+                    'msgField' => $validator->errors(),
+                ]);
+            }
+
+            UserModel::create($request->all());
+            return response()->json([
+                'status' => True, 
+                'message' => 'Data user berhasil disimpan'
+            ]);
+        }  
+        redirect('/');
+    }
+
+    public function edit_ajax(string $id)
+    {
+        $user = UserModel::find($id);
+        $level = LevelModel::select('level_id', 'level_nama')->get();
+
+        return view('user.edit_ajax', ['user' => $user, 'level' => $level]);
+    }
+
+    public function update_ajax(Request $request, $id){ 
+        // cek apakah request dari ajax 
+        if ($request->ajax() || $request->wantsJson()) { 
+            $rules = [ 
+                'level_id' => 'required|integer', 
+                'username' => 'required|max:20|unique:m_user,username,'.$id.',user_id', 
+                'nama'     => 'required|max:100', 
+                'password' => 'nullable|min:6|max:20' 
+            ]; 
+               
+        // use Illuminate\Support\Facades\Validator; 
+        $validator = Validator::make($request->all(), $rules); 
+ 
+        if ($validator->fails()) { 
+            return response()->json([ 
+                'status'   => false,    // respon json, true: berhasil, false: gagal 
+                'message'  => 'Validasi gagal.', 
+                'msgField' => $validator->errors()  // menunjukkan field mana yang error 
+            ]); 
+        } 
+ 
+        $check = UserModel::find($id); 
+        if ($check) { 
+            if(!$request->filled('password') ){ // jika password tidak diisi, maka hapus dari 
+                // request
+                $request->request->remove('password'); 
+            } 
+             
+            $check->update($request->all()); 
+            return response()->json([ 
+                'status'  => true, 
+                'message' => 'Data berhasil diupdate' 
+            ]); 
+        } else{ 
+            return response()->json([ 
+                'status'  => false, 
+                'message' => 'Data tidak ditemukan' 
+            ]); 
+        } 
+    } 
+        return redirect('/'); 
+    } 
+    
+    public function confirm_ajax(string $id)
+{
+    $user = UserModel::find($id);
+    return view('user.confirm_ajax', ['user' => $user]);
+}
+
+public function delete_ajax(Request $request, string $id)
+{
+    if ($request->ajax() || $request->wantsJson()) {
+        $user = UserModel::find($id);
+        if ($user) {
+            $user->delete();
+            return response()->json([
+                'status' => true,
+                'message' => 'Data berhasil dihapus'
+            ]);
+        } else {
+            return response()->json([
+                'status' => false,
+                'message' => 'Data tidak ditemukan'
+            ]);
+        }
+    }
+    return redirect('/');
+}
+}
