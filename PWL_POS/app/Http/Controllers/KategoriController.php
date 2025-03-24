99c83386d33441cb9cbe487262ce79b0ac7da256
febriansyah adi nugroho <febriansyahhoho@gmail.com>
2025-03-24 23:30:20 +0700

    perbaikan

--- PWL_POS/app/Http/Controllers/KategoriController.php
+++ PWL_POS/app/Http/Controllers/KategoriController.php
@@ -44,8 +44,7 @@ class KategoriController extends Controller
                     . csrf_field() . method_field('DELETE') . '
                     <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm(\'Apakah Anda yakin menghapus data ini?\');">Hapus</button>
                     </form>';*/
-                    $btn  = '<button onclick="modalAction(\''.url('/kategori/' . $kategori->kategori_id . 
-                    '/show_ajax').'\')" class="btn btn-info btn-sm">Detail</button> '; 
+                    $btn = '<a href="'.url('kategori/'.$kategori->kategori_id).'" class="btn btn-info btn-sm">Detail</a>'; 
                                 $btn .= '<button onclick="modalAction(\''.url('/kategori/' . $kategori->kategori_id . 
                     '/edit_ajax').'\')" class="btn btn-warning btn-sm">Edit</button> '; 
                                 $btn .= '<button onclick="modalAction(\''.url('/kategori/' . $kategori->kategori_id . 
--- PWL_POS/app/Http/Controllers/SupplierController.php
+++ PWL_POS/app/Http/Controllers/SupplierController.php
@@ -39,8 +39,7 @@ class SupplierController extends Controller
                     . csrf_field() . method_field('DELETE') . '
                     <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm(\'Apakah Anda yakin menghapus data ini?\');">Hapus</button>
                     </form>';*/
-                    $btn  = '<button onclick="modalAction(\''.url('/supplier/' . $supplier->supplier_id . 
-                    '/show_ajax').'\')" class="btn btn-info btn-sm">Detail</button> '; 
+                    $btn = '<a href="'.url('supplier/'.$supplier->supplier_id).'" class="btn btn-info btn-sm">Detail</a>'; 
                                 $btn .= '<button onclick="modalAction(\''.url('/supplier/' . $supplier->supplier_id . 
                     '/edit_ajax').'\')" class="btn btn-warning btn-sm">Edit</button> '; 
                                 $btn .= '<button onclick="modalAction(\''.url('/supplier/' . $supplier->supplier_id . 
